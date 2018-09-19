b505c70d4be38e59634ab8c43b332d0b3dc402cf
HeleleMama <kyyang@bu.edu>
2018-09-19 08:23:02 -0700

    read_key.py update

--- lib/read_key.py
+++ lib/read_key.py
@@ -2,7 +2,7 @@
 
 def read_key(key_filename=""):
     key_file = open(key_filename,'r')
-    keys = key_file.readlines()
+    keys = key_file.read().splitlines()
     #Order: Consumer API keys/Consumer API secret keys/Access token/access token secret
     key_file.close()
 #    print(keys)
